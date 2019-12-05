9822741e2725ce8e8d4416a46e54f8747d86d4f0
Anthony <anthony.quere@lacatholille.fr>
2019-12-05 22:17:52 +0100

    init express mysql

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mysql = require('mysql');
+
+const app = express();
+
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : ''
+});
+
+connection.connect();
+
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+    if (err) throw err;
+    console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
