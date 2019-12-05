108eff4c9f4a7a93a3b71041be4837dfd3f5c175
Anthony <anthony.quere@lacatholille.fr>
2019-12-05 23:05:49 +0100

    add first routes

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mysql = require('mysql');
-
+const bodyParser = require("body-parser");
 const app = express();
 
 var connection = mysql.createConnection({
     host     : 'localhost',
     user     : 'root',
-    password : ''
+    password : '',
+    database : 'nuit_on_fire'
 });
 
 connection.connect();
@@ -16,4 +17,17 @@ connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
     console.log('The solution is: ', rows[0].solution);
 });
 
+
+
+app.get('/qqch', function (req, res) {
+    //res.json(req.body);
+    console.log(req);
+});
+
+app.post('/newTopic', (req, res) => {
+    // req.body.
+});
+
 connection.end();
+
+app.listen(3000, () => console.log("listening on port 3000"));
