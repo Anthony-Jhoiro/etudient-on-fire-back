2d730fac034b4efc6250086a462392cf15175f89
brokiki <kilian.debraux@lacatholille.fr>
2019-12-06 02:35:39 +0100

    back pour topics

--- index.js
+++ index.js
@@ -5,29 +5,49 @@ const app = express();
 
 var connection = mysql.createConnection({
     host     : 'localhost',
+    port     : '3308', 
     user     : 'root',
     password : '',
-    database : 'nuit_on_fire'
+    database : 'entudiant-on-fire'
 });
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
-    if (err) throw err;
-    console.log('The solution is: ', rows[0].solution);
-});
+// connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+//     if (err) throw err;
+//     console.log('The solution is: ', rows[0].solution);
+// });
 
+recupListeTopic = (categorie = null) => {
+    
+}
 
-
-app.get('/qqch', function (req, res) {
+app.get('/topics', function (req, res) {
     //res.json(req.body);
-    console.log(req);
+    if (req.query.categorie) {
+        connection.query('SELECT * FROM topic', (err, rows, field) => {
+            if (err) throw err;
+            res.json(rows);
+        });
+        console.log("il est passé par ici");
+    }
+    else {
+        connection.query('SELECT id_topic, titre, content, nom, prenom, libelle FROM topic INNER JOIN user ON topic.auteur = user.id_usr INNER JOIN categorie ON topic.cat_id = categorie.id_cat', (err, rows, fiels) => {
+            if (err) throw err;
+            res.json(rows);
+        });
+        console.log("il repassera par là");
+    }
 });
 
-app.post('/newTopic', (req, res) => {
-    // req.body.
-});
+app.get('/commentaires', function (req, res){
+    
+})
 
-connection.end();
+// app.post('/newTopic', (req, res) => {
+//     // req.body.
+// });
 
-app.listen(3000, () => console.log("listening on port 3000"));
+
+
+app.listen(3001, () => console.log("listening on port 3000"));
